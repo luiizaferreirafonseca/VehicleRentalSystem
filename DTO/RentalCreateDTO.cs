697268853f87376c6e9fbf3980f2a708a5b151ea
luiizaferreirafonseca <luiizaferreirafonseca@gmail.com>
2026-02-06 21:24:00 -0300

    feat/adding rentalCreateDTO and enum

---
+++ DTO/RentalCreateDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace VehicleRentalSystem.DTO
+{
+    namespace VehicleRentalSystem.DTO
+    {
+        public class RentalCreateDTO
+        {
+            public Guid UserId { get; set; }
+            public Guid VehicleId { get; set; }
+            public DateTime StartDate { get; set; }
+            public DateTime ExpectedEndDate { get; set; }
+        }
+    }
+
+}
---
+++ Enums/EnumRentalStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace VehicleRentalSystem.Enums
+{
+    namespace VehicleRentalSystem.Enums
+    {
+        public enum RentalStatus
+        {
+            Active,
+            Completed,
+            Canceled
+        }
+    }
+
+}
