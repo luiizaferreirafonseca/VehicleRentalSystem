d4f927e2a092e5a548c84647b027ea5cee936dfd
AlessandraBatistaJ <alessandra.batista.0305@gmail.com>
2026-02-10 16:26:24 -0300

    feat: cancelamento e configuracao agnostica do banco

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using VehicleRentalSystem.Models;
 using VehicleRentalSystem.Repositories;
 using VehicleRentalSystem.Repositories.interfaces;
 using VehicleRentalSystem.Services;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<PostgresContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<PostgresContext>();
 builder.Services.AddScoped<IRentalRepository, RentalRepository>();
 builder.Services.AddScoped<IRentalService, RentalService>();
