45fca0dcab4730d97128f11eaf892a8b0b33ec2c
AlessandraBatistaJ <alessandra.batista.0305@gmail.com>
2026-02-12 14:45:35 -0300

    feat: add RatingCreateDTO for rental evaluation

---
+++ VehicleSystem/DTO/RatingCreateDTO.cs
@@ -0,0 +1,10 @@
+namespace VehicleSystem.DTO
+{
+    public class RatingCreateDTO
+    {
+        public int RentalId { get; set; }
+        public int Score { get; set; } 
+        public string? Comment { get; set; } 
+    }
+}
+
