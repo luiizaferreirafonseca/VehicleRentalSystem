e081cb7ebf365841a979937577752a1e24ee7589
Priscilla Trevizan <priscillatrevizan.dev@gmail.com>
2026-02-13 21:11:13 -0300

    feat(dto): define contratos de entrada e saída para acessórios

---
+++ VehicleSystem/DTO/AccessoryCreateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace VehicleRentalSystem.DTO;
+
+public class AccessoryCreateDto
+{
+    public string Name { get; set; } = string.Empty;
+
+    public decimal DailyRate { get; set; }
+}
---
+++ VehicleSystem/DTO/RentalAccessoryRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace VehicleRentalSystem.DTO;
+
+public class RentalAccessoryRequestDto
+{
+    public Guid RentalId { get; set; }
+
+    public Guid AccessoryId { get; set; }
+}
