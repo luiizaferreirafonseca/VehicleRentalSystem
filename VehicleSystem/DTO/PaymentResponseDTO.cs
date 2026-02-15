6175c4dc7010419081e47bac76df27ce0c8de33f
Priscilla Trevizan <priscillatrevizan.dev@gmail.com>
2026-02-15 20:56:17 -0300

    feat(dto): definir estrutura de resposta para listagem de pagamentos

---
+++ VehicleSystem/DTO/PaymentResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace VehicleRentalSystem.DTO;
+
+public class PaymentResponseDto
+{
+    public Guid Id { get; set; }
+
+    public Guid RentalId { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public string PaymentMethod { get; set; } = null!;
+
+    public DateTime PaymentDate { get; set; }
+
+}
