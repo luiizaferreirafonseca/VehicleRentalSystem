2f47d3045da5227ae71c6f27ba7e96f40c9cf298
Priscilla Trevizan <priscillatrevizan.dev@gmail.com>
2026-02-15 20:56:38 -0300

    feat(repository): implementar busca dinâmica com IQueryable e tratamento de UTC para Postgres

--- VehicleSystem/Repositories/interfaces/IPaymentRepository.cs
+++ VehicleSystem/Repositories/interfaces/IPaymentRepository.cs
@@ -7,6 +7,7 @@ namespace VehicleRentalSystem.Repositories.interfaces
         Task AddPaymentAsync(TbPayment payment);
         Task<decimal> GetTotalPaymentsAsync(Guid rentalId);
         Task<IEnumerable<TbPayment>> GetPaymentsByRentalIdAsync(Guid rentalId);
+        Task<IEnumerable<TbPayment>> GetAllPaymentsAsync(Guid? rentalId, string? method, DateTime? startDate, DateTime? endDate);
         Task SaveChangesAsync();
     }
 }
